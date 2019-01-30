97c89efb43372a39430ce9128510917e22a4c1c4
HansolChoe <hansolchoe@handong.edu>
2019-01-30 16:15:17 +0900

    Update main to turn 'initial input option' on/off
    When getting -i option, crest set environment variable
    "CREST_INITIAL_INPUT" to true, otherwise false.

--- src/run_crest/run_crest.cc
+++ src/run_crest/run_crest.cc
@@ -11,62 +11,107 @@
 #include <assert.h>
 #include <stdio.h>
 #include <sys/time.h>
+#include <getopt.h>
 
 #include "run_crest/concolic_search.h"
+struct option long_options[] =
+{
+    {"random", no_argument, 0, 0},
+    {"random_input", no_argument, 0, 0},
+    {"dfs", optional_argument, 0, 0},
+    {"cfg", no_argument, 0, 0},
+    {"cfg_baseline", no_argument, 0,0},
+    {"hybrid", no_argument, 0, 0},
+    {"uniform_random", no_argument, 0, 0},
+    {0,0,0,0}
+};
 
-int main(int argc, char* argv[]) {
-  if (argc < 4) {
+void print_help() {
     fprintf(stderr,
             "Syntax: run_crest <program> "
             "<number of iterations> "
             "-<strategy> [strategy options]\n");
     fprintf(stderr,
-            "  Strategies include: "
+            "Strategies include: "
             "dfs, cfg, random, uniform_random, random_input \n");
-    return 1;
-  }
+}
 
-  string prog = argv[1];
-  int num_iters = atoi(argv[2]);
-  string search_type = argv[3];
+int is_initial_input_option = 0;
 
-  // Initialize the random number generator.
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  srand((tv.tv_sec * 1000000) + tv.tv_usec);
-
-  crest::Search* strategy;
-  if (search_type == "-random") {
-    strategy = new crest::RandomSearch(prog, num_iters);
-  } else if (search_type == "-random_input") {
-    strategy = new crest::RandomInputSearch(prog, num_iters);
-  } else if (search_type == "-dfs") {
-    if (argc == 4) {
-      strategy = new crest::BoundedDepthFirstSearch(prog, num_iters, 1000000);
-    } else {
-      strategy = new crest::BoundedDepthFirstSearch(prog, num_iters, atoi(argv[4]));
+int main(int argc, char* argv[]) {
+    int opt;
+    int option_index = 0;
+    if (argc < 4) {
+        print_help();
+        return 1;
     }
-  } else if (search_type == "-cfg") {
-    strategy = new crest::CfgHeuristicSearch(prog, num_iters);
-  } else if (search_type == "-cfg_baseline") {
-    strategy = new crest::CfgBaselineSearch(prog, num_iters);
-  } else if (search_type == "-hybrid") {
-    strategy = new crest::HybridSearch(prog, num_iters, 100);
-  } else if (search_type == "-uniform_random") {
-    if (argc == 4) {
-      strategy = new crest::UniformRandomSearch(prog, num_iters, 100000000);
-    } else {
-      strategy = new crest::UniformRandomSearch(prog, num_iters, atoi(argv[4]));
+    
+    string search_type = "";
+    
+    // Initialize the random number generator.
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    srand((tv.tv_sec * 1000000) + tv.tv_usec);
+    
+    while((opt = getopt_long_only(argc, argv,"i", long_options, &option_index)) != EOF) {
+        switch(opt) {
+            case 0:
+                if (search_type != "") {
+                    print_help();
+                    return 1;
+                }
+                search_type = long_options[option_index].name;
+                break;
+            case 'i':
+                is_initial_input_option = 1;
+                break;
+            default:
+                print_help();
+                return 1;
+        }
     }
-  } else {
-    fprintf(stderr, "Unknown search strategy: %s\n", search_type.c_str());
-    return 1;
-  }
+    
+    if (search_type == "" || optind + 1 >= argc) {
+        print_help();
+        return 1;
+    }
+    
+    // Set emvoronment variable to on/off initial input option
+    const char *input_option_val = is_initial_input_option ? "true" : "false";
+    setenv("CREST_INITIAL_INPUT", input_option_val, 1);
+    string prog = argv[optind++];
+    int num_iters = atoi(argv[optind++]);
+    
+    crest::Search* strategy;
+    if (search_type == "random") {
+        strategy = new crest::RandomSearch(prog, num_iters);
+    } else if (search_type == "random_input") {
+        strategy = new crest::RandomInputSearch(prog, num_iters);
+    } else if (search_type == "dfs") {
+        if (!optarg) {
+            strategy = new crest::BoundedDepthFirstSearch(prog, num_iters, 1000000);
+        } else {
+            strategy = new crest::BoundedDepthFirstSearch(prog, num_iters, atoi(optarg));
+        }
+    } else if (search_type == "cfg") {
+        strategy = new crest::CfgHeuristicSearch(prog, num_iters);
+    } else if (search_type == "cfg_baseline") {
+        strategy = new crest::CfgBaselineSearch(prog, num_iters);
+    } else if (search_type == "hybrid") {
+        strategy = new crest::HybridSearch(prog, num_iters, 100);
+    } else if (search_type == "uniform_random") {
+        if (!optarg) {
+            strategy = new crest::UniformRandomSearch(prog, num_iters, 100000000);
+        } else {
+            strategy = new crest::UniformRandomSearch(prog, num_iters, atoi(optarg));
+        }
+    } else {
+        fprintf(stderr, "Unknown search strategy: %s\n", search_type.c_str());
+        return 1;
+    }
 
   strategy->Run();
 
   delete strategy;
   return 0;
 }
-
-
